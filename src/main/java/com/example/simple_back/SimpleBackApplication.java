a23f0e1435530892fd0bde63c45927ff2a68a840
Kevin1home <kschoettler@mail.de>
2025-07-16 09:38:38 +0200

    correct: a.p

--- src/main/java/com/example/simple_back/SimpleBackApplication.java
+++ src/main/java/com/example/simple_back/SimpleBackApplication.java
@@ -3,20 +3,11 @@ package com.example.simple_back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class SimpleBackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SimpleBackApplication.class);
-
-		String port = System.getenv("PORT");
-		if (port != null) {
-			app.setDefaultProperties(Collections.singletonMap("server.port", port));
-		}
-
-		app.run(args);
+		SpringApplication.run(SimpleBackApplication.class, args);
 	}
 
 }
